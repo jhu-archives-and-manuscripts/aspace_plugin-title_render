b250d5dc335646a20e6d663817fc9477f3d15e60
Tim DiLauro <tim.di.lauro+gitkraken@gmail.com>
2017-11-21 04:26:14 -0500

    use existing mixed content parser to render breadcrumbs and titles

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -6,6 +6,10 @@ ArchivesSpacePublic::Application.config.after_initialize do
   ApplicationHelper
   module ApplicationHelper
 
+    def render_mixed_content(text)
+      MixedContentParser::parse(text, '')
+    end
+
     def truncate(string, length = 50+40, trailing = '&hellip;')
       return truncate_html(string, length: length, omission: trailing)
     end
